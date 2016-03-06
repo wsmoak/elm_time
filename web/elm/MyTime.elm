25b2f527bfe1576f0b217d7086882b2ffac305b7
Wendy Smoak <wsmoak@gmail.com>
2016-03-06 16:57:33 -0500

    Add type annotations

--- web/elm/MyTime.elm
+++ web/elm/MyTime.elm
@@ -1,17 +1,19 @@
 module MyTime where
 
-import Html exposing (div, button, text)
+import Html exposing (..)
 import Html.Events exposing (onClick)
 import StartApp.Simple as StartApp
 
-main : Signal Html.Html
+main : Signal Html
 main =
   StartApp.start { model = model, view = view, update = update }
 
+type alias Model =
+  Int
 
 model = 0
 
-
+view : Signal.Address Action -> Model -> Html
 view address model =
   div []
     [ button [ onClick address Decrement ] [ text "-" ]
@@ -22,7 +24,7 @@ view address model =
 
 type Action = Increment | Decrement
 
-
+update : Action -> Model -> Model
 update action model =
   case action of
     Increment -> model + 1
